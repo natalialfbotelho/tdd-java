ed4f55f0358363f21b6d2d2ac7339be990b054ff
natalialfbotelho <engnataliabotelho@gmail.com>
2021-03-21 19:40:50 -0300

    Commit inicial

---
+++ src/test/java/com/natalia/tdd/ValidarCpfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natalia.tdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidarCpfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
