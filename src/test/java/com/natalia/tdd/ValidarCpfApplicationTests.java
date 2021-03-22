0f85b2808731a19aa15350b83a830f48f334421c
natalialfbotelho <engnataliabotelho@gmail.com>
2021-03-22 10:35:35 -0300

    Adicionando mais casos de teste

--- src/test/java/com/natalia/tdd/ValidarCpfApplicationTests.java
+++ src/test/java/com/natalia/tdd/ValidarCpfApplicationTests.java
@@ -46,5 +46,76 @@ class ValidarCpfApplicationTests {
 		cliente.setCpf("023.229.310-477");
 		assertEquals(false, cliente.validarCPF());
 	}
+			
+	@Test
+	void fazendoTesteDeCpfValidoComPontos() {
+		Cliente cliente = new Cliente();
+		cliente.setNome("Natalia");
+		cliente.setCpf("419.926.870-72");
+		assertEquals(true, cliente.validarCPF());		
+	}
+	
+	@Test
+	void fazendoTesteDeCpfInvalidoComPonto() {
+		Cliente cliente = new Cliente();
+		cliente.setNome("Natalia");
+		cliente.setCpf("419.926.870-73");
+		assertEquals(false, cliente.validarCPF());		
+	}
+	
+	@Test
+	void fazendoTesteDeCpfInvalidoComVirgula() {
+		Cliente cliente = new Cliente();
+		cliente.setNome("Natalia");
+		cliente.setCpf("419,926.870-73");
+		assertEquals(false, cliente.validarCPF());		
+	}
+	
+	@Test
+	void fazendoTesteDeCpfInvalidoComCaractereEspecial() {
+		Cliente cliente = new Cliente();
+		cliente.setNome("Natalia");
+		cliente.setCpf("419;926.870-73");
+		assertEquals(false, cliente.validarCPF());		
+	}
+	
+	@Test
+	void fazendoTesteDeCpfValidoComEspaco() {
+		Cliente cliente = new Cliente();
+		cliente.setNome("Natalia");
+		cliente.setCpf("419.926.870-72 ");
+		assertEquals(true, cliente.validarCPF());		
+	}
+	
+	@Test
+	void fazendoTesteDeCpfInvalidoComEspaco() {
+		Cliente cliente = new Cliente();
+		cliente.setNome("Natalia");
+		cliente.setCpf("419.926.870-73 ");
+		assertEquals(false, cliente.validarCPF());		
+	}
+			
+	@Test
+	void fazendoTesteDeCpfInvalidoComLetra() {
+		Cliente cliente = new Cliente();
+		cliente.setNome("Natalia");
+		cliente.setCpf("419.926.i70-72");
+		assertEquals(false, cliente.validarCPF());		
+	}
+
+	@Test
+	void fazendoTesteComCpfMalucoQuePassou() {
+		Cliente cliente = new Cliente();
+		cliente.setNome("Natalia");
+		cliente.setCpf("69b.969.790-88");
+		assertEquals(false, cliente.validarCPF());		
+	}
+
+	@Test
+	void fazendoTesteSemPassarCpf() {
+		Cliente cliente = new Cliente();
+		cliente.setNome("Natalia");		
+		assertEquals(false, cliente.validarCPF());		
+	}
 
 }
